13d29a042a2395db5430caa5f88d3c946e8776f5
Douglas Oliveira <douglas.oliveira4@outlook.com>
2020-11-30 12:20:39 -0300

    start-Projetc

---
+++ src/main/java/br/com/drink/ReceitasDrinks/ReceitasDrinksApplication.java
@@ -0,0 +1,13 @@
+package br.com.drink.ReceitasDrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitasDrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitasDrinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/drink/ReceitasDrinks/ReceitasDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drink.ReceitasDrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasDrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
