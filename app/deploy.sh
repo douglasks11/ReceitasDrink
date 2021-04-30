4402d43cbab8dd8a86dc1412c86dde7a08eec51a
Victor Augusto <44125400+vvaug@users.noreply.github.com>
2021-04-30 01:44:07 -0300

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -7,11 +7,11 @@ mvn clean install -DskipTests=true
 
 echo "Generating jar file"
 
-scp -i "~/Desktop/AWS/pem/vaugst.pem" ec2_cmd.sh ec2-user@ec2-35-175-135-95.compute-1.amazonaws.com:/home/ec2-user
+scp -i "~/Desktop/AWS/pem/vaugst.pem" ec2_cmd.sh ec2-user@ec2-54-243-47-241.compute-1.amazonaws.com:/home/ec2-user
 echo "Copied latest 'ec2_cmd.sh' file from local machine to ec2 instance"
 
-scp -i "~/Desktop/AWS/pem/vaugst.pem" target/*.jar ec2-user@ec2-35-175-135-95.compute-1.amazonaws.com:/home/ec2-user
+scp -i "~/Desktop/AWS/pem/vaugst.pem" target/*.jar ec2-user@ec2-54-243-47-241.compute-1.amazonaws.com:/home/ec2-user
 echo "Copied jar file from local machine to ec2 instance"
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
-ssh -i "~/Desktop/AWS/pem/vaugst.pem" ec2-user@ec2-35-175-135-95.compute-1.amazonaws.com ./ec2_cmd.sh
+ssh -i "~/Desktop/AWS/pem/vaugst.pem" ec2-user@ec2-54-243-47-241.compute-1.amazonaws.com ./ec2_cmd.sh
